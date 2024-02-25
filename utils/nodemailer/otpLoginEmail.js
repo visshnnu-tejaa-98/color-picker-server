8b1c22b33cb8bf79255534b7bcc1e67241a0855a
visshnnu-tejaa-98 <74036897+visshnnu-tejaa-98@users.noreply.github.com>
2024-02-25 11:52:26 +0530

    Server implement login through otp (#10)
    * Implement Login through OTP
    
    * Remove Console.log statement

---
+++ utils/nodemailer/otpLoginEmail.js
@@ -0,0 +1,8 @@
+export const otpLoginEmail = (name, otp) => `<div>
+<p>Hi ${name},</p>
+<p><strong>${otp}</strong> is OTP to login your UI-Color Picker Application, Please Do not share with anyone.</p>
+<br />
+<br />
+<p>Best Regards,</p>
+<p>Team UI-Color-Picker</p>
+</div>`;
